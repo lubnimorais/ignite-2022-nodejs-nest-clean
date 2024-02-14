389787cf3ef5c4bc8da3179ade24ce1c82a5c24f
Lubni Morais <lubni.morais@gmail.com>
2024-02-13 21:10:32 -0300

    env: add key about Cloudflare

--- src/infra/storage/rs-storage.ts
+++ src/infra/storage/rs-storage.ts
@@ -19,7 +19,7 @@ export class R2Storage implements Uploader {
     const accountId = envService.get('CLOUDFLARE_ACCOUNT_ID');
 
     this.client = new S3Client({
-      endpoint: `${accountId}`,
+      endpoint: `https://${accountId}.r2.cloudflarestorage.com`,
       region: 'auto', // CLOUDEFLARE PEDE PARA DEIXAR AUTO
       credentials: {
         accessKeyId: envService.get('AWS_ACCESS_KEY_ID'),
